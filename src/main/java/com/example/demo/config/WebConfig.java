b238ab98bf00f1b1a3edf4d11db643f4edaf54bc
stream1080 <fmxhax638@foxmail.com>
2021-06-24 21:08:15 +0800

    优化登录功能-未完成

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,41 @@
+//package com.example.demo.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.List;
+//
+///**
+// * Title: WebConfig
+// * Description: MVC配置类
+// *
+// */
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Autowired
+//    private UserArgumentResolver userArgumentResolver;
+//
+////    @Autowired
+////    private AccessInterceptor accessInterceptor;
+//
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+//        resolvers.add(userArgumentResolver);
+//    }
+//
+////    @Override
+////    public void addInterceptors(InterceptorRegistry registry) {
+////        registry.addInterceptor(accessInterceptor);
+////    }
+////
+////    @Override
+////    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+////        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+////    }
+//
+//}
