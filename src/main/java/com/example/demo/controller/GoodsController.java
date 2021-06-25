af0c3f1a809971001f242501e73cdc89894c1c3f
stream1080 <fmxhax638@foxmail.com>
2021-06-25 10:36:16 +0800

    秒杀按钮设置

--- src/main/java/com/example/demo/controller/GoodsController.java
+++ src/main/java/com/example/demo/controller/GoodsController.java
@@ -4,6 +4,8 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 import com.example.demo.service.GoodsService;
 import com.example.demo.service.UserService;
+import com.example.demo.vo.GoodsVo;
+import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 /**
  * <p>
@@ -58,7 +61,34 @@ public class GoodsController {
     @RequestMapping("/detail/{goodId}")
     public String toDetail(Model model, User user, @PathVariable long goodId){
         model.addAttribute("user",user);
-        model.addAttribute("goods",goodsService.findGoodsVoById(goodId));
+        GoodsVo goodsVo = goodsService.findGoodsVoById(goodId);
+        Date startDate = goodsVo.getStartDate();
+        Date endDate = goodsVo.getEndDate();
+        Date nowDate = new Date();
+
+        //秒杀状态
+        int secKillStatus = 0;
+        //秒杀倒计时
+        int remainSeconds = 0;
+        //秒杀结束倒计时
+        int endSeconds = (int) ((endDate.getTime() - nowDate.getTime()) / 1000);
+        //秒杀还未开始
+        if (nowDate.before(startDate)) {
+            remainSeconds = (int) ((startDate.getTime() - nowDate.getTime()) / 1000);
+        } else if (nowDate.after(endDate)) {
+            //秒杀已结束
+            secKillStatus = 2;
+            remainSeconds = -1;
+            endSeconds = 0;
+        } else {
+            //秒杀进行中
+            secKillStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("secKillStatus", secKillStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+        model.addAttribute("endSeconds", endSeconds);
+        model.addAttribute("goods",goodsVo);
         return "goodsDetail";
     }
 
